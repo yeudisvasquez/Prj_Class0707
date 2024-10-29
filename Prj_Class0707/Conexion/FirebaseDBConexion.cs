88b7adbbd7ebe485ac1cb2a0690fc79bda4150c7
yeudisvasquez <yhvherrera@gmail.com>
2024-10-29 19:45:15 -0400

    Update FirebaseDBConexion.cs
    Original code is including the API key: 
    public static FirebaseClient FireBase_Connect = new FirebaseClient("https://prj-class0707-default-rtdb.firebaseio.com/");
            public const string WepApyAuthentication = "AIzaSyBX0RJifeUSl6nr0tcRDu47k1GKHkxmAbY";

--- Prj_Class0707/Conexion/FirebaseDBConexion.cs
+++ Prj_Class0707/Conexion/FirebaseDBConexion.cs
@@ -8,7 +8,7 @@ namespace Prj_Class0707.Conexion
     public class FirebaseDBConexion
     {
         public static FirebaseClient FireBase_Connect = new FirebaseClient("https://prj-class0707-default-rtdb.firebaseio.com/");
-        public const string WepApyAuthentication = "AIzaSyBX0RJifeUSl6nr0tcRDu47k1GKHkxmAbY";
+        public const string WepApyAuthentication = "";
 
     }
 }
